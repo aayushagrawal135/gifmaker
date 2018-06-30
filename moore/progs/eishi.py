a0dd83dd9f6ec5e98e3d4476ee7143d6df4dd62e
aayushagrawal135 <aayushagrawal135@gmail.com>
2018-06-30 17:03:51 +0530

    Commit 3

--- moore/progs/eishi.py
+++ moore/progs/eishi.py
@@ -2,8 +2,7 @@ import support as sup
 import cv2
 import imageio
 
-def makegif(filenames):
-	gifname = "/home/aayush/Downloads/Curiosity/moore/set1/movie.gif"
+def makegif(filenames, gifname):
 	images = []
 	for file in filenames:
 		images.append(file)
@@ -11,19 +10,26 @@ def makegif(filenames):
 
 #---------------------------------------------------------------
 
-filename = "/home/aayush/Downloads/Curiosity/moore/set1/"
-images = []
+def wrapper(foldername, gifname, iter = 3, fps = 3):
+	images = []
 
-for i in range(8):
-	name = filename + str(i) + ".jpg"
-	img = cv2.imread(name)
-	img = sup.toGray(img)
-	images.append(img)
+	for i in range(8):
+		name = foldername + str(i) + ".jpg"
+		img = cv2.imread(name)
+		img = sup.toGray(img)
+		images.append(img)
 
-images = sup.meanSubtraction(images)
-res = []
-for i in range(8):
-	img = sup.zStretch(images[i], 3)
-	res.append(img)
+	images = sup.meanSubtraction(images)
+	res = []
+	for i in range(8):
+		img = sup.zStretch(images[i], 3)
+		res.append(img)
 
-makegif(res)
+	makegif(res, gifname)
+#-------------------------------------------------
+
+foldername = "/home/aayush/Downloads/Curiosity/moore/set1/"
+gifname = "/home/aayush/Downloads/Curiosity/moore/set1/movie.gif"
+
+#Assumed: There are atleast 8 images names as x.jpg where x lies from 0 to number of images. 
+wrapper(foldername, gifname)
